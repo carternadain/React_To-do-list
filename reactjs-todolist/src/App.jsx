7ee97df4e52e8061dd988642e73c7e84c1ed56f5
carternadain <cnadain@gmail.com>
2024-07-31 21:01:54 -0700

    add fonts / connect app

--- reactjs-todolist/src/App.jsx
+++ reactjs-todolist/src/App.jsx
@@ -1,10 +1,13 @@
+import TodoInput from "./components/Todoinput"
+import TodoList from "./components/Todolist"
+
 function App() {
 
   return (
-    <>
-
-
-     </>
+    <main>
+      <TodoInput/>
+      <TodoList/>
+     </main>
   )
 }
 
